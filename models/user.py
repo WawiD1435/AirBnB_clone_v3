dee89cbe72242cbc6a50eb81014131021e7c431f
kinfeurael wubishet <kinfeuraelwubishet@gmail.com>
2022-11-23 02:57:43 +0000

    update models

--- models/user.py
+++ models/user.py
@@ -18,7 +18,9 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        reviews = relationship(
+            "Review", backref="user", cascade="delete, all"
+        )
     else:
         email = ""
         password = ""
